623bafdd08a492f6279b3c3c56f9231ad71b5153
Shiyu <ksynja95@mail.ru>
2025-08-09 01:53:52 +0700

    Added realization findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,14 +4,16 @@ import java.util.Map;
 public class PhoneBook {
 
     private Map<String, String> contacts = new HashMap<>();
+    private final Map<String, String> numbersToNames = new HashMap<>();
 
     public String findByNumber(String number) {
-        return null;
+        return numbersToNames.get(number);
     }
 
     public int add(String name, String phoneNumber) {
         if (!contacts.containsKey(name)) {
             contacts.put(name, phoneNumber);
+            numbersToNames.put(phoneNumber, name); // <-- добавить эту строку
         }
         return contacts.size();
     }
