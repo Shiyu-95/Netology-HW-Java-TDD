07e64dbeb9c20e0ae6fb120a3522681178018f01
Shiyu <ksynja95@mail.ru>
2025-08-09 02:06:46 +0700

    Added realization findByName

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -11,7 +11,7 @@ public class PhoneBook {
     }
 
     public String findByName(String name) {
-        return null;
+        return contacts.get(name);
     }
 
     public int add(String name, String phoneNumber) {
